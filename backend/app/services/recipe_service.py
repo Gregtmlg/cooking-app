441bc2a5a3be0a3c7df0ce48aa74234538fc724a
Gregtmlg <greg.glemot@gmail.com>
2026-06-21 22:36:07 +0200

    feat: ajout services CRUD recettes et ingrédients

---
+++ backend/app/services/recipe_service.py
@@ -0,0 +1,56 @@
+from sqlalchemy.orm import Session
+
+from app.models.recipe import Recipe
+from app.models.ingredient import Ingredient
+from app.models.recipe_ingredient import RecipeIngredient
+from app.schemas.recipe import RecipeCreate, RecipeUpdate
+
+
+def get_recipe(db : Session, recipe_id : int) -> Recipe | None:
+    return db.query(Recipe).filter(Recipe.id == recipe_id).first()
+
+def get_recipes(db : Session, skip : int = 0, limit : int = 100) -> list[Recipe]:
+    return db.query(Recipe).offset(skip).limit(limit).all()
+
+def create_recipe(db: Session, recipe_in: RecipeCreate) -> Recipe:
+    db_recipe = Recipe(
+        title=recipe_in.title,
+        description=recipe_in.description,
+        instructions=recipe_in.instructions,
+        prep_time=recipe_in.prep_time,
+        cook_time=recipe_in.cook_time,
+        servings=recipe_in.servings
+    )
+    db.add(db_recipe)
+    db.flush()  # .flush() permet de générer l'ID du nouvel objet Recipe avant de l'utiliser pour créer les relations avec les ingrédients. Cela garantit que l'ID est disponible pour les relations, même si la transaction n'est pas encore validée (commit).
+    db.refresh(db_recipe)
+    return db_recipe
+
+def update_recipe(db: Session, recipe_id: int, recipe_in: RecipeUpdate) -> Recipe | None:
+    db_recipe = get_recipe(db, recipe_id)
+    if not db_recipe:
+        return None
+
+    for field, value in recipe_in.model_dump(exclude_unset=True).items():   # .model_dump(exclude_unset=True) permet de ne récupérer que les champs qui ont été modifiés dans l'objet RecipeUpdate, en excluant ceux qui n'ont pas été définis (unset). Cela permet d'éviter de réécrire des valeurs par défaut ou nulles pour les champs non modifiés. 
+        setattr(db_recipe, field, value)    # setattr(db_recipe, field, value) permet de mettre à jour dynamiquement les attributs de l'objet db_recipe avec les nouvelles valeurs fournies dans recipe_in. Cela évite d'avoir à écrire manuellement chaque champ à mettre à jour.
+
+    db.commit()
+    db.refresh(db_recipe)
+    return db_recipe
+
+def delete_recipe(db: Session, recipe_id: int) -> Recipe | None:
+    db_recipe = get_recipe(db, recipe_id)
+    if not db_recipe:
+        return None
+
+    db.delete(db_recipe)
+    db.commit()
+    return db_recipe
+
+def get_or_create_ingredient(db: Session, ingredient_name: str) -> Ingredient:
+    db_ingredient = db.query(Ingredient).filter(Ingredient.name == ingredient_name.strip().lower()).first()
+    if db_ingredient is None:
+        db_ingredient = Ingredient(name=ingredient_name.strip().lower())
+        db.add(db_ingredient)
+        db.flush()
+    return db_ingredient
