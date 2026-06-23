0340da89a5f9a4dcddf04bf3337fc4e252793e32
Gregtmlg <greg.glemot@gmail.com>
2026-06-23 19:36:53 +0200

    refactor: renommage recipe.js et fonctions à l'intérieur

--- frontend/src/api/recipes.js
+++ frontend/src/api/recipes.js
@@ -4,26 +4,26 @@ const api = axios.create({
     baseURL: 'http://localhost:8000'
 })
 
-export async function get_recipes() {
+export async function getRecipes() {
     const response = await api.get('/api/v1/recipes/')
     return response.data
 }
 
-export async function get_recipe(id) {
+export async function getRecipe(id) {
     const response = await api.get(`/api/v1/recipes/${id}/`)
     return response.data
 }
 
-export async function create_recipe(data) {
+export async function createRecipe(data) {
     const response = await api.post('/api/v1/recipes/', data)
     return response.data
 }
 
-export async function update_recipe(id, data) {
+export async function updateRecipe(id, data) {
     const response = await api.put(`/api/v1/recipes/${id}/`, data)
     return response.data
 }
 
-export async function delete_recipe(id) {
+export async function deleteRecipe(id) {
     const response = await api.delete(`/api/v1/recipes/${id}/`)
 }
