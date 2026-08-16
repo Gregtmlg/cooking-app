37f2da3f7182e20b50e11c45a279e5f5c7d86207
Gregtmlg <97436591+Gregtmlg@users.noreply.github.com>
2026-08-16 11:24:58 +0200

    Feat/home routing (#9)
    * chore: proxy /api to backend in vite dev server
    
    * feat: add home route and nest recipe pages under layout outlet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import RecipeList from './pages/RecipeList.jsx'
 import RecipeDetail from './pages/RecipeDetail.jsx'
 import RecipeCreate from './pages/RecipeCreate.jsx'
 import RecipeEdit from './pages/RecipeEdit.jsx'
+import HomePage from './pages/HomePage.jsx'
 import Layout from './components/Layout.jsx'
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route element={<Layout />}>
+        <Route path="/recipes" element={<RecipeList />} />
         <Route path="/recipes/:id" element={<RecipeDetail />} />
         <Route path="/recipes/new" element={<RecipeCreate />} />
         <Route path="/recipes/:id/edit" element={<RecipeEdit />} />
-      </Routes>
-    </Layout>
+      </Route>
+    </Routes>
   )
 }
 
